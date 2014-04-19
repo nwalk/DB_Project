7b7e0e7f41e740edd67a790f83dffcf983af56bf
nwalk <nrwalk8939@ung.edu>
2014-04-18 20:55:53 -0400

    Create table statements
    I wrote a connection class, and all the 'create table' statement to work
    with the customer/product., as well as a beginning menu. This should be
    a good foundation to develop from.

--- Database.py
+++ Database.py
@@ -1,1 +1,68 @@
+"""
+Database Module
+Nathan Walker
+nwalk19@gmail.com
+"""
+
+import psycopg2
+
 class DBConnect():
+    """Creates a database connection and a cursor"""
+    def __init__(self):
+        self.conn = psycopg2.connect("dbname=DbProject user=postgres host=localhost password=postgres")
+        self.cur = self.conn.cursor()
+        self.conn.commit()
+        print("Connected")
+
+class DBOperations(DBConnect):
+    """This class performs all the operations
+       needed to work with the database"""
+
+    def createTable(self):
+        """Creates tables appliance, customer, sales, and sold_appl"""
+        self.cur.execute("""CREATE TABLE appliance(id serial,
+                                                   type varchar,
+                                                   brand varchar,
+                                                   model varchar,
+                                                   price numeric
+                                                   );"""
+                         )
+        self.conn.commit()
+        
+        self.cur.execute("""CREATE TABLE customer(id serial,
+                                                  name varchar(20),
+                                                  phone varchar,
+                                                  address varchar
+                                                  );"""
+                         )
+        self.conn.commit()
+
+        self.cur.execute("""CREATE TABLE sales(id serial,
+                                               cust_id integer,
+                                               date timestamp,
+                                               delivery_date timestamp,
+                                               war_exp timestamp                                          
+                                               );"""
+                         )
+        self.conn.commit()
+        self.cur.execute("""CREATE TABLE sold_appl(id serial,
+                                                   cust_id integer,
+                                                   appl_id integer,
+                                                   date timestamp
+                                                   );"""
+                         )
+        self.conn.commit()
+        print("Tables Created")
+
+    def dropTables(self):
+        """Drops tables; appliance, customer, sales, and sold_appl"""
+        self.cur.execute("""DROP TABLE appliance;""")
+        self.cur.execute("""DROP TABLE customer;""")
+        self.cur.execute("""DROP TABLE sales;""")
+        self.cur.execute("""DROP TABLE sold_appl;""")
+        self.conn.commit()
+        print("Tables Dropped")
+                         
+                        
+
+
--- menu.py
+++ menu.py
@@ -1,4 +1,50 @@
+from database import DBOperations
+
+
 class Menu():
-    """Menu options for communication with the database"""
+    """Displays a menu and respond to choices when run."""
     def __init__(self):
+        self.SQL = DBOperations()
+        self.choices = {
+                1: self.create,
+                2: self.drop,
+##                3: self.,
+##                4: self.,
+##                5: self.,
+##                6: self.,
+##                7: self.,
+##                8: self.quit,
+
+    def display_menu(self):
+        print("""
+Used Appliance:
+                   MAIN MENU
+
+1 - Add Table
+2 - Drop Tables
+""")
+
+    def run(self):
+        """Display the menu and respond to choices."""
+        while True:
+            self.display_menu()
+            choice = input("Enter an option: ")
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                print("{} is not a valid choice".format(choice))
+
+    def create(self):
+        self.SQL.createTable()
+
+    def drop(self):
+        self.SQL.dropTables()
         
+        
+        
+
+
+
+if __name__ == "__main__":
+    Menu().run()
