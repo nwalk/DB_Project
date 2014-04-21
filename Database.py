59c245499a959056de54bd90621c0975c96f5599
nwalk <nrwalk8939@ung.edu>
2014-04-21 00:21:37 -0400

    developed search/sale functions
    For database.py, I have added some functions for searching the database,
    select statements by name, and phone number. Also the sale of an item
    has a lot of working parts. I have all of the tables storing data, and I
    added a menu feature to the menu.py module for searching. Saleticket1
    and 2 are the backbone to most of the data transfer in the program thus
    far. I used the "re" module to pick out multiple appliance ids for the
    purchase of more that one. i think i need some foreign keys to create
    some integrity in my tables.

--- Database.py
+++ Database.py
@@ -23,21 +23,62 @@ class DBOperations(DBConnect):
         self.cur.execute("""INSERT INTO appliance (app_type, brand, model, price)
                             VALUES (%s, %s, %s, %s);""", (a, b, m, p,))
         self.conn.commit()
+        self.cur.execute("""SELECT MAX(id) FROM appliance;""")
+        x = self.cur.fetchone()
+        print x[0]
         
-
     def addCustomer(self, n, p, a):
         self.cur.execute("""INSERT INTO customer (name, phone, address)
                             VALUES (%s, %s, %s);""", (n, p, a,))
         self.conn.commit()
 
-    def saleTicket(self, n, p):
+    def searchName(self, n):
+        self.cur.execute("""SELECT * FROM customer
+                            WHERE name = %s;""", (n,))
+        x = self.cur.fetchall()
+        for a,b,c,d in x:
+            print a,b,c,d
+        
+    def searchPhone(self, p):
+        self.cur.execute("""SELECT * FROM customer
+                            WHERE phone = (%s);""", (p,))
+        x = self.cur.fetchall()
+        for a,b,c,d in x:
+            print a,b,c,d
+
+    def saleTicket1(self, n, p, a, app):
+        self.cur.execute("""SELECT * FROM customer
+                            WHERE name = (%s)
+                            AND phone = (%s);""", (n, p,))
+        x = self.cur.fetchone()
+        print x
+        if x == None:
+            self.cur.execute("""INSERT INTO customer (name, phone, address)
+                            VALUES (%s, %s, %s);""", (n, p, a,))
+            self.conn.commit()
+            DBOperations().saleTicket2(n, p, a, app)
+        else:
+            DBOperations().saleTicket2(n, p, a, app)
+        
+    def saleTicket2(self, n, p, a, app=''):
         self.cur.execute("""SELECT id FROM customer
                             WHERE name = (%s)
-                            OR phone = (%s);""", (n, p,))
+                            AND phone = (%s);""", (n, p,))
+        x = self.cur.fetchone()
+        self.cur.execute("""INSERT INTO sales (cust_id, date)
+                            VALUES (%s, Now());""", (x,))
+        self.conn.commit()
+        self.cur.execute("""SELECT MAX(id) FROM sales;""")
+        ID = self.cur.fetchone()
+        for app_id in app:
+                self.cur.execute("""INSERT INTO sold_appl(sale_id,
+                                                          cust_id,
+                                                          appl_id,
+                                                          date)
+                                    VALUES (%s, %s, %s, Now());""",
+                                (ID, x, app_id,))
+                self.conn.commit()
         
-        
-        
-
     def createTable(self):
         """Creates tables appliance, customer, sales, and sold_appl"""
         self.cur.execute("""CREATE TABLE appliance(id serial,
